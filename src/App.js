e108a259f6077a764a1c9ef76dd59c9d61f8712a
Vamshi Maddur <vamci07@gmail.com>
2020-07-05 18:54:20 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useMediaQuery } from "@material-ui/core";
+import { ThemeWrapper, myTheme } from "./theme";
+import "./App.scss";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  background: ${(p) => p.theme.palette.background.paper};
+  height: 64px;
+  border: 1px solid ${(p) => p.theme.palette.divider};
+`;
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  let theme = React.useMemo(() => myTheme(prefersDarkMode ? "dark" : "light"), [
+    prefersDarkMode,
+  ]);
+
+  const [currentTheme, setCurrentTheme] = useState('light');
+
+  function handleThemeChange() {
+    if (currentTheme === "light") setCurrentTheme("dark");
+    else setCurrentTheme("light");
+  }
+
+  theme = React.useMemo(() => myTheme(currentTheme), [currentTheme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeWrapper theme={theme}>
+      <div className="container">
+        <div className="App">
+          <input
+            className="switch-input"
+            id="theme-switch"
+            type="checkbox"
+            onChange={handleThemeChange}
+          />
+          <label className="switch-control" for="theme-switch"></label>
+          <header className="App-header">
+            <h1>
+              Implementing Light vs Dark Mode In React Apps Using Material UI
+              and styled-components
+            </h1>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <StyledContainer>Some random text!</StyledContainer>
+          </header>
+        </div>
+      </div>
+    </ThemeWrapper>
   );
 }
 
